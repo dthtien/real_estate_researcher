5faac910073a90a695d5e3f340518fb7f6d8db67
Anthony <tien.dau@dounets.com>
2019-07-01 09:36:14 +0700

    Refactor scraping ward

--- app/models/ward.rb
+++ app/models/ward.rb
@@ -2,4 +2,14 @@ class Ward < Address
   belongs_to :district, foreign_key: :parent_id
   has_many :streets, foreign_key: :parent_id
   has_many :lands, through: :streets
+
+  def update_total_page!(page_count)
+    if persisted? && total_page != page_count
+      self.finish = false
+      self.scrapping_page = 0
+    end
+    self.total_page = page_count
+
+    save!
+  end
 end
--- app/services/scrapers/district_detail.rb
+++ app/services/scrapers/district_detail.rb
@@ -3,37 +3,46 @@ class Scrapers::DistrictDetail < Scrapers::Base
   BASE_ADDRESS_ELEMENT = '#RightMainContent__productCountByContext_bodyContainer #divCountByAreas ul a'.freeze
 
   def call
-    province_name = 'Hồ Chí Minh'.freeze
-    province = Province.find_or_create_by(
-      name: province_name,
-      alias_name: VietnameseSanitizer.execute!(province_name)
-    )
+    province = update_province
     list_urls(HCM_REAL_ESTATE_URL).map do |link|
-      distric_name = link[:name]
-      p distric_name
+      district = update_district(link: link, province: province)
       ward_links = list_urls(link[:href])
-      district = District.find_or_create_by(
-        name: distric_name,
-        alias_name: VietnameseSanitizer.execute!(distric_name),
-        parent_id: province.id
-      )
-
       ward_links.each do |ward_link|
-        ward_name = ward_link[:name]
-        ward = Ward.find_or_initialize_by(
-          name: ward_name,
-          alias_name: VietnameseSanitizer.execute!(ward_name),
-          parent_id: district.id,
-          scrapping_link: ward_link[:href]
-        )
-        page_count = total_page(page_content(ward_link[:href]))
-        ward.total_page = page_count
-        ward.save!
+        update_district(link: ward_link, district: district)
       end
     end
   end
 
   private
+
+  def update_province(province_name: 'Hồ Chí Minh'.freeze)
+    Province.find_or_create_by(
+      name: province_name,
+      alias_name: VietnameseSanitizer.execute!(province_name)
+    )
+  end
+
+  def update_district(link:, province:)
+    distric_name = link[:name]
+    p distric_name
+    District.find_or_create_by(
+      name: distric_name,
+      alias_name: VietnameseSanitizer.execute!(distric_name),
+      parent_id: province.id
+    )
+  end
+
+  def update_ward(link:, district:)
+    ward_name = link[:name]
+    ward = Ward.find_or_initialize_by(
+      name: ward_name,
+      alias_name: VietnameseSanitizer.execute!(ward_name),
+      parent_id: district.id,
+      scrapping_link: link[:href]
+    )
+
+    ward.update_total_page!(total_page(page_content(link[:href])))
+  end
 
   def list_urls(url)
     page = page_content(url)
@@ -43,6 +52,7 @@ class Scrapers::DistrictDetail < Scrapers::Base
 
   def total_page(page)
     return 0 if page.blank?
+
     last_page = page.css('.background-pager-right-controls a').last
     last_page.present? ? last_page['href'].to_s.gsub(/[^\d]+/, '').to_i : 1
   end
