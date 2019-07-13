82efba5e9fc1e1a32198c59fab897afd6dd071a7
Anthony <tien.dau@dounets.com>
2019-07-13 09:22:00 +0700

    Change deploy folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,6 @@ set :application, 'real_estate_researcher'
 set :repo_url, 'git@github.com:dthtien/real_estate_researcher.git'
 set :rails_env, 'production'
 
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 set :migration_role, :app
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 append :linked_files, 'config/database.yml'
