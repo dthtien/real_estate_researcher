5809e3ebc05edd17a3635d7e55b88f745051b082
Anthony <tien.dau@dounets.com>
2019-07-13 16:48:19 +0700

    Fix deploy env

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, 'git@github.com:dthtien/real_estate_researcher.git'
 set :user, 'deploy'
 set :puma_threads, [4, 6]
 set :puma_workers, 0
+set :rails_env, :production
 
 set :pty, true
 set :use_sudo, false
@@ -28,9 +29,6 @@ set :keep_releases, 5
 #   tmp/cache
 #   tmp/sockets
 #   .bundle
-#   public/system
-#   public/downloads
-#   public/uploads
 # ]
 set :rbenv_ruby, '2.5.1'
 
@@ -41,9 +39,6 @@ set :rbenv_ruby, '2.5.1'
 # set :log_level,     :debug
 # set :keep_releases, 5
 
-## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
--- config/deploy/virtual_box.rb
+++ config/deploy/virtual_box.rb
@@ -1,8 +1,6 @@
-role :app, 'deploy@localhost:2200'
-role :web, 'deploy@localhost:2200'
 set :user, 'deploy'
 
-server 'localhost', user: fetch(:user), roles: %w[web app], port: '2200'
+server 'localhost', user: fetch(:user), roles: %w[app db web], port: '2200'
 
 set :deploy_to, "/home/#{fetch :user}/#{fetch :application}"
 
