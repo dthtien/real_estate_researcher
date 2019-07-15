21314b7dba818b58cef5cf348e412c6c16fb41e7
Anthony <tien.dau@dounets.com>
2019-07-15 13:28:48 +0700

    Change restart puma command

--- config/deploy.rb
+++ config/deploy.rb
@@ -75,7 +75,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
+      invoke 'puma:smart_restart'
     end
   end
 
