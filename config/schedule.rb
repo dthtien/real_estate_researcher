b0e1afb0a3e4b94087619c5db603046e1cfb2e03
dthtien <tiendt2311@gmail.com>
2019-11-03 18:10:43 +0700

    deploy(crontab): change scrapping time

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,11 @@
 env :PATH, ENV['PATH']
 env :SLACK_HOOK_URL, ENV['SLACK_HOOK_URL']
-set :output, "log/cron_log.log"
+set :output, 'log/cron_log.log'
 
-every :day, at: '11:30pm', role: [:app] do
+every :day, at: '11:15am', role: [:app] do
   rake 'scrapping:start'
 end
 
-every :day, at: '12:01am', role: [:app] do
+every :day, at: '05:00pm', role: [:app] do
   rake 'logging_prices:start'
 end
