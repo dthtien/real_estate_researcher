fc171a6e24eeae4a0bf02435fcf6846766f62241
dthtien <tiendt2311@gmail.com>
2019-11-03 22:19:58 +0700

    fix(crontab): change scrapping time

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,15 +6,15 @@ set :output, 'log/cron_log.log'
 #   rake 'scrapping:start'
 # end
 
-every :day, at: '4:01pm', role: [:app] do
+every :day, at: '4:31pm', role: [:app] do
   rake 'scrapping:first_district'
 end
 
-every :day, at: '4:02pm', role: [:app] do
+every :day, at: '4:32pm', role: [:app] do
   rake 'scrapping:second_district'
 end
 
-every :day, at: '4:03pm', role: [:app] do
+every :day, at: '4:33pm', role: [:app] do
   rake 'scrapping:third_district'
 end
 
