9d3e8691fee3b2513d6b413a1b243e035aa6f804
dthtien <tiendt2311@gmail.com>
2019-11-09 17:05:43 +0700

    feat(sidekiq): sidekiq authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 require 'sidekiq/web'
 require 'sidekiq/cron/web'
 
+class AdminConstraint
+  def matches?(request)
+    request.params[:key] == ENV['SIDEKIQ_KEY']
+  end
+end
+
 Rails.application.routes.draw do
-  mount Sidekiq::Web => '/sidekiq'
+  mount Sidekiq::Web => '/sidekiq', constraints: AdminConstraint.new
 
   namespace :v1, path: 'api/v1' do
     resources :addresses, only: %i[index show] do
