f501ef768ecb1d4c7edd8d87e391608f6b698804
dthtien <tiendt2311@gmail.com>
2019-11-17 22:09:49 +0700

    fix(cron_job): fix scrapping job

--- app/jobs/scrapping_lands_job.rb
+++ app/jobs/scrapping_lands_job.rb
@@ -1,7 +1,14 @@
 class ScrappingLandsJob < ApplicationJob
-  def perform(order = 'desc')
-    District.order(created_at: order).find_each do |district|
+  DESC_SORT = 'desc'.freeze
+
+  def perform(order = DESC_SORT)
+    districts = District.all
+    scrapping = lambda do |district|
       Scrapers::LandDetail.new.call_with_district(district)
     end
+
+    districts = districts.reverse if order != DESC_SORT
+
+    districts.each(&scrapping)
   end
 end
