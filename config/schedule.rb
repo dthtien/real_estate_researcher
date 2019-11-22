97184bd8e6c1169f3903370d4a517360b5d899b4
dthtien <tiendt2311@gmail.com>
2019-11-22 20:47:21 +0700

    fix(cron): change cron time

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@ env :PATH, ENV['PATH']
 env :SLACK_HOOK_URL, ENV['SLACK_HOOK_URL']
 set :output, 'log/cron_log.log'
 
-every :day, at: '01:00am', role: [:app] do
+every :day, at: '01:35pm', role: [:app] do
   rake 'scrapping:first'
   rake 'scrapping:second'
   rake 'scrapping:third'
