4636ffb168ef4df46218e69b2c621db77b70b099
Dau The Tien <tiendt2311@gmail.com>
2020-03-24 08:36:49 +0700

    deploy: update new server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,7 @@
 set :user, 'deploy'
 
-server '40.112.62.102', user: fetch(:user), roles: %w[app db web]
+server '34.87.181.165', user: fetch(:user), roles: %w[app db web]
+# server '40.112.62.102', user: fetch(:user), roles: %w[app db web]
 
 set :deploy_to, "/home/#{fetch :user}/#{fetch :application}"
 
