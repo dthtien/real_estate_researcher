0635568dcf86620dd83507c2690b31ee8f2e6c08
Dau The Tien <tiendt2311@gmail.com>
2020-05-10 21:00:07 +0700

    feat(deploy): set cron job remove production log

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,5 @@ env :SLACK_HOOK_URL, ENV['SLACK_HOOK_URL']
 set :output, 'log/cron_log.log'
 
 every :day, at: '02:00pm', role: [:app] do
-  rake 'scrapping:first'
-  rake 'scrapping:second'
-  rake 'scrapping:third'
+  command "cd #{Rails.root} && rm log/production.log"
 end
