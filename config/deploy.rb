18592efd618e7b9d1323b31e85fe538d131b2e94
Dau The Tien <tiendt2311@gmail.com>
2020-05-10 21:59:25 +0700

    fix(deploy): update crontab config and change the way to restart puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -92,7 +92,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:smart_restart'
+      invoke 'puma:restart'
     end
   end
 
--- config/schedule.rb
+++ config/schedule.rb
@@ -3,5 +3,5 @@ env :SLACK_HOOK_URL, ENV['SLACK_HOOK_URL']
 set :output, 'log/cron_log.log'
 
 every :day, at: '02:00pm', role: [:app] do
-  command "cd #{Rails.root} && rm log/production.log"
+  command "cd ~/top_lands_api/current && rm log/production.log"
 end
